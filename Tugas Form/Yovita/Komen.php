0a06b3d17653b776e2c323e6c60574bb9e6c20bd
yovitamargaret <yovitaabigail2013@gmail.com>
2020-10-20 21:37:17 +0700

    init commit

--- Tugas Form/Yovita/Komen.php
+++ Tugas Form/Yovita/Komen.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <title>ASPIRA-SI</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <link rel="stylesheet" href="../assets/css/bootstrap.css">
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../../assets/css/bootstrap.css">
+    <link rel="stylesheet" href="../../assets/css/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Commissioner:wght@700&family=Roboto&display=swap"
           rel="stylesheet">
 </head>
@@ -47,6 +47,7 @@
                     <div class="row">
                         <div class="col-9">
                             <h3>Proses Registrasi surat izin lambat</h3>
+                            <span class="span-time">Posted on September, 23rd 2020. 15.34pm</span>
                         </div>
                         <div class="col-3 text-right">
                             <button class="btn btn-sm btn-outline-danger">Down Vote</button>
@@ -58,7 +59,7 @@
                     <div class="aspiration-card-body-content">
                         <div class="row mb-4">
                             <div class="col-1 col">
-                                <img alt="" class="img-thumbnail img-icon" src="../assets/img/telkom.jpg"
+                                <img alt="" class="img-thumbnail img-icon" src="../../assets/img/telkom.jpg"
                                      style="width: 50px">
                             </div>
                             <div class="col-11 col">
@@ -88,11 +89,32 @@
                 <div class="card-footer aspiration-card-footer">
                     <div class="row">
                         <div class="col-1 col">
-                            <img alt="" class="img-thumbnail img-icon" src="../assets/img/telkom.jpg"
+                            <img alt="" class="img-thumbnail img-icon" src="../../assets/img/telkom.jpg"
                                  style="width: 50px;">
                         </div>
                         <div class="col-11 col">
-                            <input class="form-control aspiration-comments" placeholder="add comments" type="text">
+                        <?php
+                            if(isset($_POST['comment'])){
+                                echo "***4151";
+                                $comment=$_POST['comment'];
+
+                                $lama=fopen("comments.txt","r+t");
+                                $komen_lama=fread($lama,1024);
+
+                                $write = fopen("comments.txt","w+");
+                                fwrite($write, $comment);
+                                fclose($write);
+                                fclose($lama);
+                            }
+
+                            $read=fopen("comments.txt","r+t");
+                            echo "<br><br>Comments<hr>".fread($read,1024);
+                            fclose($read);
+
+                            ?> <br><br>
+                            <form id="commentform" action="" method="POST">
+                            <input autocomplete="off" class="form-control aspiration-comments" placeholder="add comments" type="text" name="comment">
+                            </form>
                         </div>
                         <div class="col-12 text-right">
                             <a href="">see more comments</a>
@@ -105,9 +127,37 @@
     <!--End of Aspiration Card-->
 </section>
 
-<button class="btn btn-primary feb btn-lg">
+<button class="btn btn-primary feb btn-lg" data-target="#exampleModalCenter" data-toggle="modal">
     +
 </button>
+
+
+<!--MODAL ADD-->
+<div aria-hidden="true" aria-labelledby="exampleModalCenterTitle" class="modal fade" id="exampleModalCenter"
+     role="dialog" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Add Aspiration</h5>
+                <button aria-label="Close" class="close" data-dismiss="modal" type="button">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="">
+                    <label for="aspirasi">Aspirasi</label>
+                    <input class="form-control" id="aspirasi" placeholder="Tuliskan Aspirasi" type="text">
+                    <label for="file">File Pendukung</label>
+                    <input class="form-control-file" id="file" multiple type="file">
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-secondary" data-dismiss="modal" type="button">Close</button>
+                <button class="btn btn-primary" type="button">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
@@ -118,5 +168,10 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
         integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
         crossorigin="anonymous"></script>
+
+
+<script type="text/javascript">
+    
+</script>
 </body>
 </html>
