da5d4f856bf40b2d2b9c5c5ae172e249ed9dabad
zhafirdhiaulhaq <62381344+zhafirdhiaulhaq@users.noreply.github.com>
2020-10-20 20:41:16 +0700

    Init Commit

--- Tugas Form/Zhafir/posting.php
+++ Tugas Form/Zhafir/posting.php
@@ -5,10 +5,17 @@
     <meta charset="UTF-8">
     <title>ASPIRA-SI</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <link rel="stylesheet" href="../assets/css/bootstrap.css">
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../../assets/css/bootstrap.css">
+    <link rel="stylesheet" href="../../assets/css/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Commissioner:wght@700&family=Roboto&display=swap"
         rel="stylesheet">
+
+    
+    <!-- jquery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- bootstrap js -->
+    <script type= "text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 </head>
 
 <body>
@@ -39,8 +46,47 @@
         </div>
     </nav>
 
+    <!--MODAL ADD-->
+    <div aria-hidden="true" aria-labelledby="exampleModalCenterTitle" class="modal fade" id="exampleModalCenter"
+        role="dialog" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">Add Aspiration</h5>
+                    <button aria-label="Close" class="close" data-dismiss="modal" type="button">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <!-- disini -->
+                    <form action="#" method="post" enctype="multipart/form-data">
+                        <label for="judul">Topik Aspirasi</label>
+                        <input class="form-control " id="judul" placeholder="Fasilitas Praktikum" type="text" name="a">
+                        <label for="kepada">Kepada</label>
+                        <input class="form-control" id="kepada" placeholder="BPMSI/Lab X/Dosen X" type="text" name="b">
+                        <label for="aspirasi">Aspirasi</label>
+                        <textarea class="form-control" name="c" id="aspirasi" cols="30" rows="3" placeholder="Mohon maaf, PC di integra ada yang rusak"></textarea>
+                        <label for="fil">File Pendukung</label>
+                        <input class="form-control-file" id="fil" multiple type="file" name="file[]">
 
-    <section class="container">
+                    <!-- sampe sini -->
+                </div>
+                <div class="modal-footer">
+                    <form method="post" action="#">
+                        <button class="btn btn-secondary" data-dismiss="modal" type="button">Close</button>
+                        <button class="btn btn-primary" type="submit" name="save" id="btn_upload">Save changes</button>
+                        
+                    </form>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php
+
+    if(isset($_POST['save'])){ ?>
+        <section class="container">
         <!--Start of Aspiration Card-->
         <div class="row justify-content-center mb-4">
             <div class="col-12">
@@ -48,8 +94,9 @@
                     <div class="card-header aspiration-card-header">
                         <div class="row">
                             <div class="col-9">
-                                <h3>Proses Registrasi surat izin lambat</h3>
-                                <span class="span-time">Posted on September, 23rd 2020. 15.34pm</span>
+                                <h3><?= $_POST['a']; ?></h3>
+                                <!-- Posted on September, 23rd 2020. 15.34pm  'l, M dS Y.', time()-->
+                                <span class="span-time">Posted on <?= date('l, M dS Y. h:ia'); ?></span>
                             </div>
                             <div class="col-3 text-right">
                                 <button class="btn btn-sm btn-outline-danger">Down Vote</button>
@@ -58,12 +105,10 @@
                         </div>
                     </div>
                     <div class="card-body aspiration-card-body">
-                        <!-- putih -->
                         <div class="aspiration-card-body-content">
-                            <!-- abu -->
                             <div class="row mb-4">
                                 <div class="col-1 col">
-                                    <img alt="" class="img-thumbnail img-icon" src="../assets/img/telkom.jpg"
+                                    <img alt="" class="img-thumbnail img-icon" src="../../assets/img/telkom.jpg"
                                         style="width: 50px">
                                 </div>
                                 <div class="col-11 col">
@@ -72,22 +117,37 @@
                                     </span>
                                     <br>
                                     <span class="span-tujuan">
-                                        Kepada: Laboratorium A
+                                        Kepada: <?= $_POST['b']; ?>
                                     </span>
                                 </div>
                             </div>
                             <div class="row justify-content-center">
                                 <div class="col-10">
-                                    <p>
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam doloremque
-                                        enim
-                                        facere iure perferendis placeat quam repellendus vel? Amet atque consequuntur
-                                        est
-                                        exercitationem expedita illum, labore molestiae nisi velit voluptatum.
-                                    </p>
+                                    <p><?= $_POST['c']; ?></p>
                                 </div>
                                 <div class="col-10">
-                                    <button class="btn btn-outline-info">File Pendukung</button>
+
+                                    <!-- <button class="btn btn-outline-info">File Pendukung</button> -->
+                                    <!-- <span>File Pendukung</span> -->
+                                    <!-- <button class="btn btn-outline-info">  -->
+                                        <?php
+                                            $countfiles = count($_FILES['file']['name']);
+                                            for($i=0; $i < $countfiles; $i++){
+                                                $filename = $_FILES['file']['name'][$i];
+                                                $dir="upload/";
+
+                                                $uploaded = move_uploaded_file($_FILES['file']['tmp_name'][$i], 'upload/'.$filename);
+                                                
+                                                if ($uploaded) {
+                                                    // echo "Upload berhasil!<br/>";
+                                                // echo "<a target=_blank href='".$dir.$filename."'>".$filename."</a>";//link _blank
+                                                echo "<img src='".$dir.$filename."' width=\"200px\">";
+                                                } else {
+                                                    echo "Upload Gagal!";
+                                                }
+                                            }                                            
+                                        ?>
+                                    <!-- </button> -->
                                 </div>
                             </div>
                         </div>
@@ -95,7 +155,7 @@
                     <div class="card-footer aspiration-card-footer">
                         <div class="row">
                             <div class="col-1 col">
-                                <img alt="" class="img-thumbnail img-icon" src="../assets/img/telkom.jpg"
+                                <img alt="" class="img-thumbnail img-icon" src="../../assets/img/telkom.jpg"
                                     style="width: 50px;">
                             </div>
                             <div class="col-11 col">
@@ -112,39 +172,12 @@
         <!--End of Aspiration Card-->
     </section>
 
+    <?php } ?>
+
+
     <button class="btn btn-primary feb btn-lg" data-target="#exampleModalCenter" data-toggle="modal">
         +
     </button>
-
-
-    <!--MODAL ADD-->
-    <div aria-hidden="true" aria-labelledby="exampleModalCenterTitle" class="modal fade" id="exampleModalCenter"
-        role="dialog" tabindex="-1">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLongTitle">Add Aspiration</h5>
-                    <button aria-label="Close" class="close" data-dismiss="modal" type="button">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <!-- disini -->
-                    <form action="">
-                        <label for="aspirasi">Aspirasi</label>
-                        <input class="form-control" id="aspirasi" placeholder="Tuliskan Aspirasi" type="text">
-                        <label for="file">File Pendukung</label>
-                        <input class="form-control-file" id="file" multiple type="file">
-                    </form>
-                    <!-- sampe sini -->
-                </div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" data-dismiss="modal" type="button">Close</button>
-                    <button class="btn btn-primary" type="button">Save changes</button>
-                </div>
-            </div>
-        </div>
-    </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
