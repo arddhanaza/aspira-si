affdfad1dd41f36dc406994521cd381585ec929e
nurhidaylma <ilmanurhidayati752@gmail.com>
2020-11-02 12:56:00 +0700

    add tampilan login

--- resources/views/Login/login.blade.php
+++ resources/views/Login/login.blade.php
@@ -2,24 +2,46 @@
 
 @section('title','Login')
 
-@section('container')
+@section('container')    
+    <div class="container-fluid">
+    <div class="row">
+    <div class="col-md-6" style="margin-top: 10%;">
+        <div class="row">
+            <div class="col-md-8 d-flex justify-content-end" style="margin-left: 4%">
+                <h1>ASPIRA <br>-SI</h1>
+            </div>            
+        </div>        
 
-    <div class="card container mt-5 col-6">
-        <div class="card-body">
-            <form action="{{route('login')}}" method="post">
-                @csrf
-                <label for="username">
-                    Username
-                </label>
-                <input type="text" name="username" class="form-control" id="username">
-                <label for="password">
-                    Password
-                </label>
-                <input type="password" name="password" id="password" class="form-control">
-                <br>
-                <button type="submit"  class="btn btn-primary btn-block">Login</button>
-            </form>
+        <div class="row">
+            <div class="col-md-10 d-flex justify-content-end">
+                <form action="{{route('login')}}" method="post">
+                    @csrf
+                    <div class="form-group">
+                        <label for="username">NIM</label>
+                        <input type="text" class="form-control shadow p-3 mb-1 bg-white rounded" id="username" name="username">
+                    </div>                
+                    <div class="form-group">
+                        <label for="password">PASSWORD</label>
+                        <input type="password" name="password" class="form-control shadow p-3 mb-1 bg-white rounded" id="password">
+                    </div>                                
+                    <button type="submit"  class="btn btn-primary btn-block shadow p-1 mb-1 ">Login</button>
+                </form>                
+            </div>            
+        </div>         
+        
+        <div class="row">
+            <div class="col-md-9 d-flex justify-content-end">
+                <a href="forgot-password.blade.php">Forget Password</a>
+            </div>
         </div>
+    </div>    
+
+    <div class="col-md-6">
+        <img src="{{URL::to('/assets/img/icon.png')}}" alt="icon aspirasi" width="70%">
     </div>
+    </div>
+    </div>
+            
+
 
 @endsection
