dcaba4035f54c294ea2ee4c9285ac9dbd542f0c4
Arddhana Zhafran Amanullah <arddhanaza99@gmail.com>
2020-11-04 21:05:11 +0700

    update js

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -211,7 +211,24 @@
     @endforeach
 
 
-    <script src="{{asset('assets/dist/js/feedsSort.js')}}">
+    <script !src="">
+        document.getElementById('sortByOption').addEventListener('change', function (e) {
+            if (e.target.value == 'teratas') {
+                refreshTeratas();
+            } else {
+                refreshTerbaru();
+            }
+        })
+
+        function refreshTerbaru() {
+            let url = "{{ route('feed')}}";
+            document.location.href = url;
+        }
+
+        function refreshTeratas() {
+            let url = "{{ route('feedPopular')}}";
+            document.location.href = url;
+        }
     </script>
     @if(session(0)->getTable() != 'bpm' or session(0)->getTable() != 'entitas_si')
         <script src="{{asset('assets/dist/js/vote.js')}}">
