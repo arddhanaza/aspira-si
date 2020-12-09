443697e1248a106fd1b4ba192316a99f1dc58fcc
yovitamargaret <60781945+yovitamargaret@users.noreply.github.com>
2020-12-09 14:36:13 +0700

    update bug

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -25,7 +25,7 @@ class ReplyController extends Controller
 
     }
     public function delete($id_aspirasi){
-        DB::table('reply')->where('reply.id_aspirasi')->delete();
+        ReplyAspiration::where('reply.id_mahasiswa','=',session(0)->id_mahasiswa)->where('reply.id_aspirasi','=',$id_aspirasi)->delete();
         return redirect('feed');
     }
 }
