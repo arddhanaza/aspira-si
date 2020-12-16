8e95e0e4973d78899eabc5f6abd98f77ed80b2d4
nurhidaylma <ilmanurhidayati752@gmail.com>
2020-12-16 10:21:28 +0700

    add store function

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Announcement;
 use App\Aspiration;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AnnouncementController extends Controller
 {
@@ -27,12 +28,28 @@ class AnnouncementController extends Controller
 
     public function store(Request $request){
         $announcement = new Announcement();
+        $aspirasi = new Aspiration();
         $announcement->id_entitas = $request->id_entitas;
         if (isset($request->judul_aspirasi)){
             $announcement->id_aspirasi = $request->judul_aspirasi;
         }
-        $announcement->announcement_text = $request->announcement_text;
-        $announcement->save();
+        if($request->hasfile('file_name'))
+        {
+            foreach($request->file('file_name') as $file)
+            {
+                $name = time().'.'.$file->extension();
+                $file->move(public_path().'/files/', $name);
+                $data[] = $name;
+            }
+            $announcement->nama_file = json_encode($data);
+        }
+        $announcement->announcement_text = $request->announcement_text;                
+        $announcement->save();    
+
+        $id_aspirasi = $request->judul_aspirasi;
+        DB::table('aspirasi')
+            -> where('id_aspirasi', '=', $id_aspirasi)
+            -> update(['status' => 'Done Resolved']);
 
         return redirect(route('announcement'));
     }
